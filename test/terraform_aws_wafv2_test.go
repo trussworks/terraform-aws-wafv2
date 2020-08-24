51e9e28f080787b81a28f792f8306b49bac1766a
Rupinder Dhariwal <rdhariwal@users.noreply.github.com>
2020-08-24 09:53:54 -0400

    Update test/terraform_aws_wafv2_test.go
    Co-authored-by: Michael Kania <dynamike@truss.works>

--- test/terraform_aws_wafv2_test.go
+++ test/terraform_aws_wafv2_test.go
@@ -77,7 +77,7 @@ func TestTerraformAwsWafv2Headers(t *testing.T)  {
 	terraform.InitAndApply(t, terraformOptions)
 }
 
-func TestTerraformAwsWafv2ipratelimit(t *testing.T)  {
+func TestTerraformAwsWafv2IpRateLimit(t *testing.T)  {
 	t.Parallel()
 
 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/ip_rate_limiting")
