6d3b281fec5359d512be3a2a82d36e37b2cdb922
Chikara Takahashi <chtakahashi10@gmail.com>
2023-03-14 13:30:54 -0400

    make response_code optional with an explicit 401 default

--- variables.tf
+++ variables.tf
@@ -69,7 +69,7 @@ variable "ip_sets_rule" {
     priority      = number
     ip_set_arn    = string
     action        = string
-    response_code = optional(number)
+    response_code = optional(number, 401)
   }))
   description = "A rule to detect web requests coming from particular IP addresses or address ranges."
   default     = []
@@ -81,7 +81,7 @@ variable "ip_rate_based_rule" {
     priority      = number
     limit         = number
     action        = string
-    response_code = optional(number)
+    response_code = optional(number, 401)
   })
   description = "A rate-based rule tracks the rate of requests for each originating IP address, and triggers the rule action when the rate exceeds a limit that you specify on the number of requests in any 5-minute time span"
   default     = null
@@ -93,7 +93,7 @@ variable "ip_rate_url_based_rules" {
     priority              = number
     limit                 = number
     action                = string
-    response_code         = optional(number)
+    response_code         = optional(number, 401)
     search_string         = string
     positional_constraint = string
   }))
